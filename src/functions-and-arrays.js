344b51d6add45d6cd0e875713c77d100732d7d1d
sridhar-dev <85818985+sridhar-dev@users.noreply.github.com>
2021-07-22 10:10:43 +0530

    Update functions-and-arrays.js

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -24,17 +24,19 @@ function findScaryWord(words){
 
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-function netPrice(numbers){
-  if(numbers.length === 0){
-    return 0;
+
+function netPrice(numbers){ 
+  if(numbers===0){
+    return 0
   }
-  var sum = 0;
-  for(var i=0; i<numbers.length; i++)
-  {
-    sum += numbers[i];
-  }
-  return sum;
+var sum = 0;
+for (var i = 0; i < numbers.length; i++) {
+  sum = sum + numbers[i];
 }
+return sum;
+}
+
+
 
 // Progression #4: Calculate the average
 // Progression 4.1: Array of numbers
@@ -44,7 +46,7 @@ function midPointOfLevels(numbersAvg){
     return null;
   }
   var avg = netPrice(numbersAvg);
-  avg = avg / (numbersAvg.length)
+  avg /= (numbersAvg.length)
   return avg;
  
 }
@@ -52,7 +54,16 @@ function midPointOfLevels(numbersAvg){
 
 // Progression 4.2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
-
+function averageWordLength(wordsArr){
+  if(wordsArr.length === 0){
+    return null;
+  }
+  var avg = 0;
+  for(var i = 0; i<wordsArr.length; i++ ){
+  avg += wordsArr[i].length
+  }
+  return (avg / wordsArr.length);
+}
 
 // Progression #5: Unique arrays
 const wordsUnique = [
@@ -70,9 +81,24 @@ const wordsUnique = [
   'flour'
 ];
 
+function uniqueArray(wordsUnique){
+  if(wordsUnique.length === 0)
+    return null;
+  return [...new Set(wordsUnique)]
+}
+
 // Progression #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
+function searchElement(wordsFind){
+  if(wordsFind.length === 0)
+    return null;
+    for(var i = 0; i<wordsFind.length; i++){
+      if(wordsFind[i]===wordsFind[i]){
+        return true;
+      }
+    }
+}
 // Progression #7: Count repetition
 const wordsCount = [
   'machine',
@@ -87,18 +113,18 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+function howManyTimesElementRepeated(wordsCount , item){
+  if(wordsCount.length === 0 ){
+  return 0;
+  }
+  var turns = 0;
+  for(var i = 0; i<wordsCount.length; i++){
+    if (wordsCount[i] === item)
+    {
+      turns+=1;
+    }
+  }
+  return turns;
+}
 
-// Progression #8: Bonus
 
-const matrix = [
-  [08, 02, 22, 97, 38, 15, 00, 40, 00, 75],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71],
-  [52, 70, 95, 23, 04, 60, 11, 42, 69, 24],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92],
-  [24, 47, 32, 60, 99, 03, 45, 02, 44, 75],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
-  [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
-  [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
-  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
-];
